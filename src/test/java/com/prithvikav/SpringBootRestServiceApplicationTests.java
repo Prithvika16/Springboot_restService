83ff03d49f6dabf825b3e006cfbec9e28f207ed0
prithvika <prithvika.v@ascendion.com>
2025-07-09 11:42:23 +0530

    Updated integration tests and fixed JSON assertion

--- src/test/java/com/prithvikav/SpringBootRestServiceApplicationTests.java
+++ src/test/java/com/prithvikav/SpringBootRestServiceApplicationTests.java
@@ -10,13 +10,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,133 +24,144 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import com.prithvika.SpringBootRestServiceApplication;
-
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prithvika.SpringBootRestServiceApplication;
 import com.prithvika.controller.AddResponse;
 import com.prithvika.controller.Library;
 import com.prithvika.controller.LibraryController;
 import com.prithvika.repository.LibraryRepository;
 import com.prithvika.service.LibraryService;
 
+import java.util.ArrayList;
+
 @SpringBootTest(classes = SpringBootRestServiceApplication.class)
 @AutoConfigureMockMvc
 class SpringBootRestServiceApplicationTests {
 
-	@Autowired
-	LibraryController con;
+    @Autowired
+    LibraryController con;
 
-	@MockBean
-	LibraryRepository repository;
+    @MockBean
+    LibraryRepository repository;
 
-	@MockBean
-	LibraryService libraryService;
+    @MockBean
+    LibraryService libraryService;
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	void contextLoads() {}
+    @BeforeEach
+    public void setup() {
+        Library book = buildLibrary();
+        when(libraryService.getBookById(book.getId())).thenReturn(book);
+        when(repository.findAllByAuthor("Prithvika")).thenReturn(List.of(book, book));
+    }
 
-	@Test
-	public void checkBuildIDLogic() {
-		LibraryService lib = new LibraryService();
-		String id = lib.buildId("ZMAN", 24);
-		assertEquals(id, "OLDZMAN24");
-		String id1 = lib.buildId("MAN", 24);
-		assertEquals(id1, "MAN24");
-	}
+    @Test
+    void contextLoads() {}
 
-	@Test
-	public void addBookTest() {
-		Library lib = buildLibrary();
-		when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
-		when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(false);
-		when(repository.save(any())).thenReturn(lib);
+    @Test
+    public void checkBuildIDLogic() {
+        LibraryService lib = new LibraryService();
+        String id = lib.buildId("ZMAN", 24);
+        assertEquals("OLDZMAN24", id);
 
-		ResponseEntity response = con.addBookImplementation(buildLibrary());
-		System.out.println(response.getStatusCode());
+        String id1 = lib.buildId("MAN", 24);
+        assertEquals("MAN24", id1);
+    }
 
-		assertEquals(response.getStatusCode(), HttpStatus.CREATED);
-		AddResponse ad = (AddResponse) response.getBody();
-		assertEquals(lib.getId(), ad.getId());
-		assertEquals("Success Book is Added", ad.getMsg());
-	}
+    @Test
+    public void addBookTest() {
+        Library lib = buildLibrary();
+        when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
+        when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(false);
+        when(repository.save(any())).thenReturn(lib);
 
-	@Test
-	public void addBookControllerTest() throws Exception {
-		Library lib = buildLibrary();
-		ObjectMapper map = new ObjectMapper();
-		String jsonString = map.writeValueAsString(lib);
+        ResponseEntity<?> response = con.addBookImplementation(lib);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
 
-		when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
-		when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(false);
-		when(repository.save(any())).thenReturn(lib);
+        AddResponse ad = (AddResponse) response.getBody();
+        assertEquals(lib.getId(), ad.getId());
+        assertEquals("Success Book is Added", ad.getMsg());
+    }
 
-		this.mockMvc.perform(post("/addBook").contentType(MediaType.APPLICATION_JSON)
-				.content(jsonString))
-			.andDo(print())
-			.andExpect(status().isCreated())
-			.andExpect(jsonPath("$.id").value(lib.getId()));
-	}
+    @Test
+    public void addBookControllerTest() throws Exception {
+        Library lib = buildLibrary();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(lib);
 
-	
-//	@Test
-//	public void updateBookTest() throws Exception {
-//		Library lib = buildLibrary();
-//		ObjectMapper map = new ObjectMapper();
-//		String jsonString = map.writeValueAsString(UpdateLibrary());
-//
-//		when(libraryService.getBookById(any())).thenReturn(buildLibrary());
-//
-//		this.mockMvc.perform(put("/updateBook/" + lib.getId()).contentType(MediaType.APPLICATION_JSON)
-//				.content(jsonString))
-//			.andDo(print())
-//			.andExpect(status().isOk())
-//			.andExpect(content().json("{\"book_name\":\"Boot\",\"id\":\"sfe3b\",\"isbn\":\"sfe\",\"aisle\":322,\"author\":\"Prithvika\"}"));
-//	}
-	
-//	@Test
-//	public void getAuthorNameBooksTest() throws Exception {
-//	    this.mockMvc.perform(get("/getBooks/author").param("authorname", "Prithvika"))
-//	        .andExpect(status().isOk())
-////	        .andExpect(jsonPath("$.length()", is(1))) // Maybe more than 1 returned
-////	        .andExpect(jsonPath("$[0].aisle").value(322)); // This might fail
-//	}
+        when(libraryService.buildId(lib.getIsbn(), lib.getAisle())).thenReturn(lib.getId());
+        when(libraryService.checkBookAlreadyExist(lib.getId())).thenReturn(false);
+        when(repository.save(any())).thenReturn(lib);
 
+        this.mockMvc.perform(post("/addBook")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").value(lib.getId()));
+    }
 
-	@Test
-	public void deleteBookControllerTest() throws Exception {
-		when(libraryService.getBookById(any())).thenReturn(buildLibrary());
-		doNothing().when(repository).delete(buildLibrary());
+    @Test
+    public void updateBookTest() throws Exception {
+        Library lib = buildLibrary();
+        Library updated = UpdateLibrary();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(updated);
 
-		this.mockMvc.perform(delete("/deleteBook").contentType(MediaType.APPLICATION_JSON)
-				.content("{\"id\" : \"sfe3b\"}"))
-			.andDo(print())
-			.andExpect(status().isCreated())
-			.andExpect(content().string("Book is deleted"));
-	}
+        when(libraryService.getBookById(lib.getId())).thenReturn(lib);
+        when(repository.save(any())).thenReturn(updated);
 
-	
+        this.mockMvc.perform(put("/updateBook/" + lib.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.book_name").value("Boot"));
+    }
 
-	public Library buildLibrary() {
-		Library lib = new Library();
-		lib.setAisle(322);
-		lib.setBook_name("Spring");
-		lib.setIsbn("sfe");
-		lib.setAuthor("Prithvika");
-		lib.setId("sfe3b");
-		return lib;
-	}
+    @Test
+    public void getAuthorNameBooksTest() throws Exception {
+        this.mockMvc.perform(get("/getBooks/author")
+                .param("authorname", "Prithvika"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()", is(2)))
+            .andExpect(jsonPath("$[0].author").value("Prithvika"));
+    }
 
-	public Library UpdateLibrary() {
-		Library lib = new Library();
-		lib.setAisle(322);
-		lib.setBook_name("Boot");
-		lib.setIsbn("rain");
-		lib.setAuthor("Prithvika");
-		lib.setId("rain322");
-		return lib;
-	}
+    @Test
+    public void deleteBookControllerTest() throws Exception {
+        when(libraryService.getBookById(any())).thenReturn(buildLibrary());
+        doNothing().when(repository).delete(any());
+
+        this.mockMvc.perform(delete("/deleteBook")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\" : \"sfe3b\"}"))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(content().string("Book is deleted"));
+    }
+
+    public Library buildLibrary() {
+        Library lib = new Library();
+        lib.setAisle(322);
+        lib.setBook_name("Spring");
+        lib.setIsbn("sfe");
+        lib.setAuthor("Prithvika");
+        lib.setId("sfe3b");
+        return lib;
+    }
+
+    public Library UpdateLibrary() {
+        Library lib = new Library();
+        lib.setAisle(322);
+        lib.setBook_name("Boot");
+        lib.setIsbn("rain");
+        lib.setAuthor("Prithvika");
+        lib.setId("sfe3b");
+        return lib;
+    }
 }
